935c8c7c3b558143d9115f42935082c0beea1c71
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-06-16 12:10:34 -0500

    List data print in html.

--- js/sheetExcelController.js
+++ js/sheetExcelController.js
@@ -3,12 +3,24 @@ workSheetApp.controller('SheetController', ['$scope', function workSheetApp($sco
 	$scope.cell = 'Hoja';
 	$scope.data = [];
 
-	for (var row = 0; row < 100; row++) {
-		var rowContent = [];
-		for (var col = 0; col < 100; col++) {
-			rowContent.push(row * col + col);
-		}
-		$scope.data.push(rowContent);
-	 }
+	$scope.datos = [
+		{num_emp:"1", nombre:"Thomas", paterno: 'Santiago', materno:'Gonzalez',
+		edad:27, genero:'M', curp:'XXXX000000XXXXXXXX', rfc:'XXXX000000XXX', 
+		nss:'XXXXXXXXXXX', fecha_nac:'2014-01-01', puesto:'Programador',
+		tipo_contrato:'Permanente', departamento:'MyCloud', tipo_nomina:'Quincenal',
+		email:'thom.gonzalez@gmail.com',
+		},
+	];
+
+	for (var i=0; i < 20; i++) {
+		angular.forEach($scope.datos, function(value) {
+		$scope.data.push([
+			value.num_emp, value.nombre, value.paterno, value.materno, value.edad,
+				value.genero, value.fecha_nac, value.tipo_nomina, value.tipo_contrato,
+				value.puesto, value.departamento, value.email
+			]);
+		});
+	};
+
 
 }]);
