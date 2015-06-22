9d5f58f5858147ba8351221d9f77aa2dbc955f22
Tomas Santiago <thom.sgonzalez@gmail.com>
2015-06-22 10:55:34 -0500

    Functional demo.

--- js/sheetExcelController.js
+++ js/sheetExcelController.js
@@ -1,10 +1,10 @@
 var workSheetApp = angular.module('workSheetApp', ['ngcTableDirective']);
 workSheetApp.controller('SheetController', ['$scope', function workSheetApp($scope, ngcTableDirective) {
-	$scope.cell = 'Hoja';
+	$scope.sheet = 'NameSheet';
 	$scope.data = [];
 
 	$scope.datos = [
-		{num_emp:"1", nombre:"Thomas", paterno: 'Santiago', materno:'Gonzalez',
+		{num_emp:"1", name:"Thomas", paterno: 'Santiago', materno:'Gonzalez',
 		edad:27, genero:'M', curp:'XXXX000000XXXXXXXX', rfc:'XXXX000000XXX', 
 		nss:'XXXXXXXXXXX', fecha_nac:'2014-01-01', puesto:'Programador',
 		tipo_contrato:'Permanente', departamento:'MyCloud', tipo_nomina:'Quincenal',
@@ -15,7 +15,7 @@ workSheetApp.controller('SheetController', ['$scope', function workSheetApp($sco
 	for (var i=0; i < 20; i++) {
 		angular.forEach($scope.datos, function(value) {
 		$scope.data.push([
-			value.num_emp, value.nombre, value.paterno, value.materno, value.edad,
+			value.num_emp, value.name, value.paterno, value.materno, value.edad,
 				value.genero, value.fecha_nac, value.tipo_nomina, value.tipo_contrato,
 				value.puesto, value.departamento, value.email
 			]);
