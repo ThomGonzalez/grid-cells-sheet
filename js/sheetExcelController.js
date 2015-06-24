69168b438c202f54aac5ec3e2a350b8d040ca017
Tomas Santiago <thom.sgonzalez@gmail.com>
2015-06-24 16:06:48 -0500

    Dynamic data, adding headers and mapping keys.

--- js/sheetExcelController.js
+++ js/sheetExcelController.js
@@ -1,43 +1,56 @@
 var workSheetApp = angular.module('workSheetApp', ['ngcTableDirective']);
 workSheetApp.controller('SheetController', ['$scope', function workSheetApp($scope, ngcTableDirective) {
-	$scope.sheet = 'NameSheet';
 	$scope.data = [];
 	$scope.header = [
-		'Empleado','Nombre','Paterno','Materno','Edad','Genero','Curp',
+		'No','Nombre','Paterno','Materno','Edad','Genero','Curp',
 		'Rfc','Nss','Fecha nacimiento','Puesto','Tipo Contrato','Departamento',
-		'Tipo Nómina', 'email'
+		'Tipo Nómina', 'Email'
 	];
-
 	$scope.datos = [
-		{num_emp:"1", nombre:"Thomas", paterno: 'Santiago', materno:'Gonzalez',
+		{num_emp:"1", nombre:"Thomas", paterno: 'Santiago', materno:null,
 		edad:27, genero:'M', curp:'XXXX000000XXXXXXXX', rfc:'XXXX000000XXX', 
 		nss:'XXXXXXXXXXX', fecha_nac:'2014-01-01', puesto:'Programador',
 		tipo_contrato:'Permanente', departamento:'MyCloud', tipo_nomina:'Quincenal',
-		email:'thom.gonzalez@gmail.com',
-		},
+		email:'thom.sgonzalez@gmail.com',
+		},{num_emp:"2", nombre:"Thom", paterno: null, materno:null,
+		edad:27, genero:null, curp:null, rfc:null, 
+		nss:null, fecha_nac:null, puesto:null,
+		tipo_contrato:null, departamento:null, tipo_nomina:null,
+		email:null,
+		}
 	];
-	
-	var keys = _.keys($scope.datos[0]);
 
-
-	var headers = [];
-	_.each($scope.header, function(value){
-		headers.push([value]);
-	});
-	// Add hearder
-	console.log(headers)
-	$scope.data.push(headers)
-
-	//for (var i=0; i < 20; i++) {
-		_.each($scope.datos, function(value, key){
-			console.log(value, key);
-		/*$scope.data.push([
-				value.num_emp, value.name, value.paterno, value.materno, value.edad,
-				value.genero, value.fecha_nac, value.tipo_nomina, value.tipo_contrato,
-				value.puesto, value.departamento, value.email
-			]);*/
+	$scope.init = function(){
+		$scope.sheet = 'NameSheet';
+		var data = data_cells();
+		$scope.data = header_cells(data);
+	};
+		
+	// Add header
+	function header_cells(data){
+		var headers = [];
+		_.each($scope.header, function(value){
+			headers.push([value]);
 		});
-	//};
+		data.unshift(headers);
+		return data;
+	};
 
+	var keys = _.keys($scope.datos[0]);
+	// Parse  
+	function data_cells(){
+		var values = [];
+		_.each($scope.datos, function(value, key){
+			var row = [];
+			_.each(keys, function(field){
+				var cells = [$scope.datos[key][field]];
+				row.push(cells);
+			});
+			values.push(row);
+		});
+		return values;
+	};
+
+	$scope.init();
 
 }]);
